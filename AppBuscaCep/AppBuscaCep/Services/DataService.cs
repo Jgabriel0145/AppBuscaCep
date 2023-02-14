ad7c13fa5f4eb8b9f57c0a17ac2fcec75508e737
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-02-14 15:07:20 -0300

    .

--- AppBuscaCep/AppBuscaCep/Services/DataService.cs
+++ AppBuscaCep/AppBuscaCep/Services/DataService.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+using Newtonsoft.Json;
+
+using AppBuscaCep.Model;
 
 namespace AppBuscaCep.Services
 {
     public class DataService
     {
+        public static async Task<Endereco> GetEnderecoByCep(string cep)
+        {
+            Endereco end;
+
+            using (HttpClient client = new HttpClient()) 
+            {
+                HttpResponseMessage response = await client.GetAsync("https:/cep.metoda.com.br/endereco/by-cep?cep=" + cep);
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    end = JsonConvert.DeserializeObject<Endereco>(json);
+                }
+                else 
+                    throw new Exception(response.RequestMessage.Content.ToString());
+            }
+
+            return end;
+        }
     }
 }
