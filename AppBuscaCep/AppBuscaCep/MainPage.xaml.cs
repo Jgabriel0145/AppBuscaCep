018f1c24ab4a3319654ceb49a163003feb4679b8
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-03-07 14:40:26 -0300

    Api

--- AppBuscaCep/AppBuscaCep/MainPage.xaml.cs
+++ AppBuscaCep/AppBuscaCep/MainPage.xaml.cs
@@ -20,13 +20,66 @@ namespace AppBuscaCep
             Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Inicial)));
         }
 
+        private async void BtnInicio_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Inicial)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
         private async void BtnEnderecoByCep_Clicked(object sender, EventArgs e)
         {
             try
             {
-
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(EnderecoPorCep)));
+                IsPresented = false;
             }
-            catch(Exception ex)
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
+        private async void BtnBairrosByCidade_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(BairrosPorCidade)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
+        private async void BtnLogradouroByBairroAndCidade_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(LogradourosByBairroAndCidade)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
+        private async void BtnCepByLogradouro_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(CepPorLogradouro)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
             {
                 await DisplayAlert("Ops!", ex.Message, "OK");
             }
