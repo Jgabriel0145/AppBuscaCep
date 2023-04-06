f9f0b7d2896ad567d1860c3f6a83703bac1b2009
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-04-06 15:54:57 -0300

    Atualização

--- AppBuscaCep/AppBuscaCep/Views/CidadePorEstado.xaml.cs
+++ AppBuscaCep/AppBuscaCep/Views/CidadePorEstado.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AppBuscaCep.Model;
+using AppBuscaCep.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,42 @@ namespace AppBuscaCep.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CidadePorEstado : ContentPage
     {
+        ObservableCollection<Cidade> lista_cidades = new ObservableCollection<Cidade>();
+
         public CidadePorEstado()
         {
             InitializeComponent();
+
+            pck_estado.ItemsSource = lista_cidades;
+        }
+
+        private async void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Picker disparador = sender as Picker;
+                string estado_selecionado = disparador.SelectedItem as string;
+
+                if (estado_selecionado != null)
+                {
+                    carregando.IsRunning = true;
+
+                    List<Cidade> arr_cidades = await DataService.GetCidadeByEstado(estado_selecionado);
+                    lista_cidades.Clear();
+
+                    arr_cidades.ForEach(i => lista_cidades.Add(i));
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+
+            }
+            finally
+            {
+                carregando.IsRunning = false;
+            }
         }
     }
 }
